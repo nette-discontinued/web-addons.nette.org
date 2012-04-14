9307a5e42b964448064f1a6b0c3878ca43e35adc
Jan Marek <mail@janmarek.net>
2012-04-14 17:41:53 +0200

    Homepage tags, recenty updated plugins

--- app/model/Tags.php
+++ app/model/Tags.php
@@ -12,9 +12,18 @@ use Nette;
 class Tags extends Table
 {
 
+	const LEVEL_CATEGORY = 1;
+	const LEVEL_SUBCATEGORY = 2;
+	const LEVEL_ORDINARY_TAG = 9;
+
 	/**
 	 * @var string
 	 */
 	protected $tableName = 'tag';
+
+	public function findMainTags()
+	{
+		return $this->findAll()->where('level = ?', self::LEVEL_CATEGORY);
+	}
 
 }
