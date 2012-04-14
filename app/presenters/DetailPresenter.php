d5c73e827c18c212a404c59b8cb2891f107cb5d9
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-04-14 17:39:50 +0200

    DetailPresenter: fix for not existing addon

--- app/presenters/DetailPresenter.php
+++ app/presenters/DetailPresenter.php
@@ -11,6 +11,7 @@ class DetailPresenter extends BasePresenter
 	public function renderDefault($id)
 	{
 		$addon = $this->context->addons->find($id);
+		if (!$addon) $this->error('Addon not found!');
 		$this->template->addon = $addon;
 	}
 
