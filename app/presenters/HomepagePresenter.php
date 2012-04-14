018f9dddb27c3066378b9b36b62a9def5109c857
Jan Marek <mail@janmarek.net>
2012-04-14 17:41:53 +0200

    Homepage tags, recenty updated plugins

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,4 +5,13 @@ namespace NetteAddons;
 class HomepagePresenter extends BasePresenter
 {
 
+	public function renderDefault()
+	{
+		$addons = $this->context->addons;
+
+		$this->template->updatedAddons = $addons->findAll()->order('updated_at DESC')->limit(3);
+
+		$this->template->categories = $this->context->tags->findMainTags();
+	}
+
 }
