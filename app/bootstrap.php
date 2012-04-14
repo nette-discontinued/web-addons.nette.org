44aeb1532c96464493b3f47bde89fda6612c40cc
Filip Procházka <hosiplan@gmail.com>
2012-04-14 17:13:51 +0200

    fixed ugly urls

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -32,7 +32,10 @@ $container = $configurator->createContainer();
 
 // Setup router
 $container->router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
-$container->router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+$container->router[] = new Route('<presenter>[/<action>][/<id>]', array(
+	'presenter' => 'Homepage',
+	'action' => 'default'
+));
 
 
 // Configure and run the application!
