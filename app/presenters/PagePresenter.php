f142289378e9bab8e4bbe6bd793a370e074e1b0d
chemiX <iam@chemix.cz>
2012-04-14 23:53:22 +0200

    add Pages

---
+++ app/presenters/PagePresenter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NetteAddons;
+
+/**
+ * @author Jan Cerny
+ */
+class PagePresenter extends BasePresenter
+{
+
+	public function renderHowToAddPlugin()
+	{
+		
+	}
+
+
+
+	public function renderPluginWritingGuidelines()
+	{
+		
+	}
+
+
+
+	public function renderSendFeedback()
+	{
+		
+	}
+
+}
