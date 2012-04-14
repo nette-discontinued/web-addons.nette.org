28f7e1e215df4cb9cd4b9c4f34eb75f35614885c
Jan Marek <mail@janmarek.net>
2012-04-15 01:46:37 +0200

    signInForm inherits from BaseForm

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -2,38 +2,16 @@
 
 namespace NetteAddons;
 
-use Nette\Application\UI,
-	Nette\Security as NS;
+use Nette\Security as NS;
 
-
-/**
- * Sign in/out presenters.
- *
- * @author     John Doe
- * @package    MyApplication
- */
 class SignPresenter extends BasePresenter
 {
 
-
-	/**
-	 * Sign in form component factory.
-	 * @return Nette\Application\UI\Form
-	 */
 	protected function createComponentSignInForm()
 	{
-		$form = new UI\Form;
-		$form->addText('username', 'Username:')
-			->setRequired('Please provide a username.');
-
-		$form->addPassword('password', 'Password:')
-			->setRequired('Please provide a password.');
-
-		$form->addCheckbox('remember', 'Remember me on this computer');
-
-		$form->addSubmit('send', 'Sign in');
-
+		$form = new SignInForm();
 		$form->onSuccess[] = callback($this, 'signInFormSubmitted');
+
 		return $form;
 	}
 
