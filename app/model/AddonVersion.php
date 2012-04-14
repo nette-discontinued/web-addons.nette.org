e023aa61a79c89207190ff6952b2af5d9c837492
Filip Procházka <hosiplan@gmail.com>
2012-04-14 17:42:21 +0200

    hydration of Addon

--- app/model/AddonVersion.php
+++ app/model/AddonVersion.php
@@ -17,4 +17,34 @@ class AddonVersion extends Nette\Object
 	 */
 	public $version;
 
+	/**
+	 * @var array|string[]
+	 */
+	public $require = array();
+
+	/**
+	 * @var array|string[]
+	 */
+	public $suggest = array();
+
+	/**
+	 * @var array|string[]
+	 */
+	public $provide = array();
+
+	/**
+	 * @var array|string[]
+	 */
+	public $replace = array();
+
+	/**
+	 * @var array|string[]
+	 */
+	public $conflict = array();
+
+	/**
+	 * @var array|string[]
+	 */
+	public $recommend = array();
+
 }
--- app/model/Tags.php
+++ app/model/Tags.php
@@ -3,6 +3,7 @@
 namespace NetteAddons\Model;
 
 use Nette;
+use Nette\Database\Table\ActiveRow;
 
 
 
@@ -21,9 +22,49 @@ class Tags extends Table
 	 */
 	protected $tableName = 'tag';
 
+
+	/**
+	 * @return \Nette\Database\Table\Selection
+	 */
 	public function findMainTags()
 	{
 		return $this->findAll()->where('level = ?', self::LEVEL_CATEGORY);
+	}
+
+
+	/**
+	 * @param \Nette\Database\Table\ActiveRow $addon
+	 * @param string|\Nette\Database\Table\ActiveRow $tag
+	 */
+	public function addAddonTag(ActiveRow $addon, $tag)
+	{
+		try {
+			if (!$tag instanceof ActiveRow) {
+				$tag = $this->getTable()
+					->where('name = ? OR slug = ? OR id = ?', $tag, $tag, $tag)
+					->limit(1)->fetch();
+			}
+
+			$this->getAddonTags()->insert(array(
+				'addonId' => $addon->getPrimary(),
+				$tag->getPrimary()
+			));
+
+			return TRUE;
+
+		} catch (\PDOException $e) {
+			return FALSE;
+		}
+	}
+
+
+
+	/**
+	 * @return \Nette\Database\Table\Selection
+	 */
+	protected function getAddonTags()
+	{
+		return $this->database->table('addon_tag');
 	}
 
 }
