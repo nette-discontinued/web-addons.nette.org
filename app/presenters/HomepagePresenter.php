daa69c0d749a8e027e953bdc3c013c42a6a71d77
Patrik Votocek <patrik@votocek.cz>
2012-04-15 01:20:54 +0200

    Add reinstall button

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -12,4 +12,23 @@ class HomepagePresenter extends BasePresenter
 		$this->template->updatedAddons = $addons->findAll()->order('updated_at DESC')->limit(3);
 	}
 
+	public function handleReinstall()
+	{
+		$connection = $this->getContext()->databaseConnection;
+
+		//$connection->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, TRUE);
+
+		$tables = $connection->getSupplementalDriver()->getTables();
+		foreach ($tables as $table) {
+			$connection->exec('SET foreign_key_checks = 0');
+			$connection->exec("DROP TABLE `{$table['name']}`");
+		}
+
+		\Nette\Database\Helpers::loadFromFile($connection, APP_DIR . "/model/db/schema.sql");
+		\Nette\Database\Helpers::loadFromFile($connection, APP_DIR . "/model/db/data.sql");
+
+		$this->flashMessage('Yea!');
+		$this->redirect('this');
+	}
+
 }
