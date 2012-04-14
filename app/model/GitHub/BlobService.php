0b89481fef1087e1fb5ae4ff9080af6b1358f360
Patrik Votocek <patrik@votocek.cz>
2012-04-14 15:59:59 +0200

    GitHub: add BlobService

---
+++ app/model/GitHub/BlobService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NetteAddons\Model\GitHub;
+
+/**
+ * @author	Patrik Votoček
+ */
+class BlobService extends \Nette\Object
+{
+	const METHOD_GET = 'GET';
+
+	/** @var string */
+	private $baseUrl;
+	/** @var \NetteAddons\Curl */
+	private $curl;
+
+	/**
+	 * @param \NetteAddons\Curl
+	 * @param string
+	 */
+	public function __construct(\NetteAddons\Curl $curl, $baseUrl = 'https://raw.github.com')
+	{
+		$this->baseUrl = $baseUrl;
+		$this->curl = $curl;
+	}
+
+	/**
+	 * @param string
+	 * @param string
+	 * @return stdClass|NULL
+	 * @throws \NetteAddons\NotImplementedException
+	 */
+	public function exec($path, $method = self::METHOD_GET)
+	{
+		$url = new \Nette\Http\Url($this->baseUrl);
+		$url->setPath($path);
+
+		if ($method != self::METHOD_GET) {
+			throw new \NetteAddons\NotImplementedException;
+		}
+
+		$data = $this->curl->get($url);
+		return $this->responseToJson($data);
+	}
+}
