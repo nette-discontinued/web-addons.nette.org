419e056d71ff09b135f56a899a3ca2b62007dc56
Jan Marek <mail@janmarek.net>
2012-04-14 20:31:38 +0200

    create composer json
    addon from activerow factory

--- app/model/Addons.php
+++ app/model/Addons.php
@@ -39,41 +39,6 @@ class Addons extends Table
 
 
 	/**
-	 * @param int $id
-	 * @return Addon|boolean
-	 */
-	public function get($id)
-	{
-		if (!$row = $this->getTable()->get($id)) {
-			return FALSE;
-		}
-
-		$addon = new Addon();
-		$addon->name = $row->name;
-		foreach ($this->findAddonVersions($row) as $versionRow) {
-			$addon->versions[] = $version = new AddonVersion();
-			$version->version = $versionRow->version;
-
-			foreach ($this->findVersionDependencies($versionRow) as $dependency) {
-				if ($dependency->package_name) {
-					$version->{$dependency->type}[$dependency->package_name] = $dependency->package_version;
-
-				} else {
-					$version->{$dependency->type}[$dependency->dependency->name] = $dependency->package_version;
-				}
-			}
-		}
-
-		foreach ($this->findAddonTags($row) as $tagRow) {
-			$addon->tags[] = $tagRow->tag->name;
-		}
-
-		return $addon;
-	}
-
-
-
-	/**
 	 * @param \Nette\Database\Table\ActiveRow $addonVersion
 	 * @return \Nette\Database\Table\GroupedSelection
 	 */
