923bce7a0220ccd237440247a4bb0ee599d7d836
Patrik Votocek <patrik@votocek.cz>
2012-04-14 15:50:05 +0200

    add small cUrl wrapper to misc

---
+++ app/misc/Curl.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace NetteAddons;
+
+/**
+ * @author	Patrik Votoček
+ */
+class Curl extends \Nette\Object
+{
+	const UA = 'Nette Addons';
+
+	/** @var int */
+	private $timeout;
+
+	/**
+	 * @param int
+	 */
+	public function __construct($timeout = 500)
+	{
+		if (!extension_loaded('curl')) {
+			throw new \NetteAddons\InvalidStateException('Missing cURL extension');
+		}
+
+		$this->timeout = $timeout;
+	}
+
+	/**
+	 * @return resource
+	 */
+	protected function create()
+	{
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_USERAGENT, self::UA);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_TIMEOUT_MS, $this->timeout);
+
+		return $ch;
+	}
+
+	/**
+	 * @param string|\Nette\Http\Url
+	 * @return string
+	 * @throws InvalidStateException
+	 */
+	public function get($url)
+	{
+		$ch = $this->create();
+
+		curl_setopt($ch, CURLOPT_URL, (string)$url);
+		curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
+
+		$data = curl_exec($ch);
+
+		if ($err = curl_errno($ch) != 0) {
+			throw new \NetteAddons\InvalidStateException("cURL error #$err ".curl_error($ch));
+		}
+
+		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if ($httpCode != 200) {
+			throw new \NetteAddons\InvalidStateException("Server returns $httpCode code");
+		}
+
+		curl_close($ch);
+
+		return $data;
+	}
+}
--- app/model/GitHub/Service.php
+++ app/model/GitHub/Service.php
@@ -7,46 +7,24 @@ namespace NetteAddons\Model\GitHub;
  */
 class Service extends \Nette\Object
 {
-	const UA = 'Nette Addons';
-
 	const METHOD_GET = 'GET';
 
 	/** @var string */
 	private $baseUrl;
-	/** @var int */
-	private $timeout;
+	/** @var \NetteAddons\Curl */
+	private $curl;
 
 	/**
+	 * @param \NetteAddons\Curl
 	 * @param string
-	 * @param int timeout in ms
 	 */
-	public function __construct($baseUrl = 'https://api.github.com', $timeout = 50000)
+	public function __construct(\NetteAddons\Curl $curl, $baseUrl = 'https://api.github.com')
 	{
-		if (!extension_loaded('curl')) {
-			throw new \NetteAddons\InvalidStateException('Missing cURL extension');
-		}
-
 		$this->baseUrl = $baseUrl;
-		$this->timeout = $timeout;
+		$this->curl = $curl;
 	}
 
-	/**
-	 * @param string|\Nette\Http\Url
-	 * @return resource
-	 */
-	protected function createCurl($url)
-	{
-		$ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_URL, (string)$url);
-		curl_setopt($ch, CURLOPT_USERAGENT, self::UA);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-		curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_TIMEOUT_MS, $this->timeout);
-
-		return $ch;
-	}
 
 	/**
 	 * @param string
@@ -78,21 +56,7 @@ class Service extends \Nette\Object
 			throw new \NetteAddons\NotImplementedException;
 		}
 
-		$ch = $this->createCurl($url);
-
-		$data = curl_exec($ch);
-
-		if ($err = curl_errno($ch) != 0) {
-			throw new \NetteAddons\InvalidStateException("cURL error #$err ".curl_error($ch));
-		}
-
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		if ($httpCode != 200) {
-			throw new \NetteAddons\InvalidStateException("GitHub returns $httpCode code");
-		}
-
-		curl_close($ch);
-
+		$data = $this->curl->get($url);
 		return $this->responseToJson($data);
 	}
 }
