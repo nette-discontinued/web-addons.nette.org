543a028dd33f38ccc512aa019ea5fb4080a2118c
Filip Procházka <hosiplan@gmail.com>
2012-04-14 14:43:40 +0200

    Added table Tags

---
+++ app/model/Tags.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NetteAddons\Model;
+
+use Nette;
+
+
+
+/**
+ * @author Filip Procházka <filip.prochazka@kdyby.org>
+ */
+class Tags extends Table
+{
+
+	/**
+	 * @var string
+	 */
+	protected $tableName = 'tags';
+
+}
