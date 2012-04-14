1723d9ab3d634a21058b842f2a1cced4173cd06e
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-04-14 20:05:40 +0200

    $categories are set in BasePresenter

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,8 +10,6 @@ class HomepagePresenter extends BasePresenter
 		$addons = $this->context->addons;
 
 		$this->template->updatedAddons = $addons->findAll()->order('updated_at DESC')->limit(3);
-
-		$this->template->categories = $this->context->tags->findMainTags();
 	}
 
 }
