379eda254aa69b73f530c7480d9c924296a5df48
Jan Smitka <jan@smitka.org>
2012-04-15 03:24:48 +0200

    Version creation - archive validation by file extension.

--- app/forms/AddVersionForm.php
+++ app/forms/AddVersionForm.php
@@ -2,6 +2,9 @@
 
 namespace NetteAddons;
 
+use Nette\Utils\Strings;
+
+
 
 /**
  * Form for addon version creation.
@@ -13,12 +16,24 @@ class AddVersionForm extends BaseForm
 		$this->addText('version', 'Version', 10, 20)
 			->addRule(self::FILLED);
 		$this->addUpload('archive', 'Archive')
-			->addRule(self::FILLED)
-			->addRule(self::MIME_TYPE, 'Only ZIP files are accepted.', array('application/zip'));
+			->addRule(self::FILLED);
 		$this->addText('license', 'License', 20, 100)
 			->addRule(self::FILLED);
 
 
 		$this->addSubmit('create', 'Create');
+
+		$this->onValidate[] = callback($this, 'validateArchive');
+	}
+
+
+	public function validateArchive(AddVersionForm $form)
+	{
+		/** @var $file \Nette\Http\FileUpload */
+		$file = $form['archive']->getValue();
+		if (!Strings::endsWith($file->getName(), '.zip')) {
+			$form['archive']->addError('Only ZIP files are allowed.');
+			$form->valid = FALSE;
+		}
 	}
 }
