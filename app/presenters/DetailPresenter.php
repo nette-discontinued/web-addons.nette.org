6d42c8129a90716dafe00a668a9a04859bd3545f
Jan Marek <mail@janmarek.net>
2012-04-15 02:31:31 +0200

    download links

--- app/presenters/DetailPresenter.php
+++ app/presenters/DetailPresenter.php
@@ -15,9 +15,17 @@ class DetailPresenter extends BasePresenter
 
 	public function renderDefault($id)
 	{
-		$addon = $this->context->addons->find($id);
-		if (!$addon) $this->error('Addon not found!');
+		$addons = $this->context->addons;
+		$addon = $addons->find($id);
+
+		if (!$addon) {
+			$this->error('Addon not found!');
+		}
+
 		$this->template->addon = $addon;
+		$this->template->registerHelper('downloadlink', function ($version) use ($addons, $addon) {
+			return $addons->getZipUrl($addon, $version);
+		});
 	}
 
 	/**
