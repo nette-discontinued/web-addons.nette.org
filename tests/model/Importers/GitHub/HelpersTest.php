139fbd6b533a8140b216b6d235ea9db36bf97f24
Patrik Votocek <patrik@votocek.cz>
2012-04-15 13:12:48 +0200

    GitHub: move normalize repository URL to helpers

---
+++ tests/model/Importers/GitHub/HelpersTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace NetteAddons\Test;
+
+/**
+ * @author	Patrik Votoček
+ */
+class GitHubHelpersTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @return array
+	 */
+	public function dataNormalizeGitHubUrl()
+	{
+		return array(
+			array('https://github.com/nette/nette', 'https://github.com/nette/nette'),
+			array('http://github.com/nette/nette', 'https://github.com/nette/nette'),
+			array('github.com/nette/nette', 'https://github.com/nette/nette'),
+			array('https://github.com/nette/nette/commits/master', 'https://github.com/nette/nette'),
+			array('https://github.com/nette/nette.git', 'https://github.com/nette/nette'),
+			array('git://github.com/nette/nette.git', 'https://github.com/nette/nette'),
+		);
+	}
+
+	/**
+	 * @dataProvider dataNormalizeGitHubUrl
+	 * @param string
+	 * @param string
+	 */
+	public function testNormalizeGitHubUrl($input, $expected)
+	{
+		$this->assertEquals(
+			$expected, \NetteAddons\Model\Importers\GitHub\Helpers::normalizeRepositoryUrl($input),
+			"normalize: $input"
+		);
+	}
+
+}
