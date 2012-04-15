b1c9a9bfbc42dbbcfb257bd7dc8defecf28284a7
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-04-15 13:49:54 +0200

    ImportAddonForm: added url validation

--- app/forms/ImportAddonForm.php
+++ app/forms/ImportAddonForm.php
@@ -12,7 +12,8 @@ class ImportAddonForm extends BaseForm
 	protected function buildForm()
 	{
 		$this->addText('url', 'Repository URL', 60, 256)
-			->addRule(self::FILLED);
+			->setRequired()
+			->addRule(self::URL, 'Invalid GitHub URL.');
 
 		$this->addSubmit('import', 'Import');
 	}
