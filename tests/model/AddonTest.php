65e566acb66abb2cc116a0e5a78db99b9ffdf188
Jan Marek <mail@janmarek.net>
2012-04-15 12:51:52 +0200

    addon composer name test

---
+++ tests/model/AddonTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NetteAddons;
+
+use NetteAddons\Model\Addon;
+
+/**
+ * @author Jan Marek
+ */
+class AddonTest extends \PHPUnit_Framework_TestCase
+{
+
+	/** @var Addon */
+	private $object;
+
+	protected function setUp()
+	{
+		$this->object = new Addon();
+	}
+
+	public function testBuildComposerName()
+	{
+		$this->object->name = 'Muj Plugínek';
+		$author = (object) array(
+			'name' => 'Honzík Marků',
+		);
+		$expected = 'HonzikMarku/MujPluginek';
+		$this->object->buildComposerName($author);
+
+		$this->assertEquals($expected, $this->object->composerName);
+	}
+
+}
