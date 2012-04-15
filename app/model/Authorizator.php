64a32ee951a8b35fc13a0ccb69d93e8f89111cba
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-04-15 04:22:48 +0200

    Authorizator: added support for ActiveRow

--- app/model/Authorizator.php
+++ app/model/Authorizator.php
@@ -26,9 +26,14 @@ class Authorizator extends Nette\Object
 	 * @param  mixed
 	 * @param  string for example 'view', 'edit'
 	 * @return bool
+	 * @throws \NetteAddons\InvalidArgumentException
 	 */
 	public function isAllowed($resource, $action)
 	{
+		if ($resource instanceof Nette\Database\Table\ActiveRow) {
+			$resource = $this->fromActiveRow($resource);
+		}
+
 		if ($resource instanceof Addon) {
 			if ($action === 'view') {
 				return TRUE;
@@ -44,6 +49,22 @@ class Authorizator extends Nette\Object
 			if ($action === 'create') {
 				return $this->user->isLoggedIn();
 			}
+		}
+
+		throw new \NetteAddons\InvalidArgumentException();
+	}
+
+	/**
+	 * Convers ActiveRow to entity
+	 *
+	 * @param  Nette\Database\Table\ActiveRow
+	 * @return mixed
+	 * @throws \NetteAddons\InvalidArgumentException
+	 */
+	private function fromActiveRow(Nette\Database\Table\ActiveRow $row)
+	{
+		if ($row->getTable()->getName() === 'addon') {
+			return Addon::fromActiveRow($row);
 		}
 
 		throw new \NetteAddons\InvalidArgumentException();
