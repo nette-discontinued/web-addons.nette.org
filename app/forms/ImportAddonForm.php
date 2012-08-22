4f01b05fe5ad09aba5fbecbb8b956363b775ccfd
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-08-22 10:23:33 +0200

    ImportAddonForm: autofocus for url field

--- app/forms/ImportAddonForm.php
+++ app/forms/ImportAddonForm.php
@@ -12,6 +12,7 @@ class ImportAddonForm extends BaseForm
 	protected function buildForm()
 	{
 		$this->addText('url', 'Repository URL', 60, 256)
+			->setAttribute('autofocus', TRUE)
 			->setRequired();
 
 		$this->addSubmit('import', 'Import');
