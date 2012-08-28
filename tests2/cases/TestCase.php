13f624c7a21641b1e4c07c2f1194a9a76e6551a4
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-08-28 16:31:20 +0200

    Tests: added Mockery

--- tests2/cases/TestCase.php
+++ tests2/cases/TestCase.php
@@ -3,10 +3,15 @@
 namespace NetteAddons\Test;
 
 use PHPUnit_Framework_TestCase;
+use Mockery;
 
 
 
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
-
+	protected function tearDown()
+	{
+		parent::tearDown();
+		Mockery::close();
+	}
 }
