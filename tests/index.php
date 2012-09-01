3d73c22c5b4d40e5dd20f89a83f6494357717dba
Patrik Votocek <patrik@votocek.cz>
2012-09-01 17:44:50 +0200

    tests: rename boot.php to bootstrap.php

--- tests/index.php
+++ tests/index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/libs/Access/Init.php';
 
 $http = new HttpPHPUnit\Main(__DIR__ . '/libs/PHPUnit');
 
-require_once __DIR__ . '/boot.php';
+require_once __DIR__ . '/bootstrap.php';
 
 $cvg = $http->coverage(__DIR__ . '/../app', __DIR__ . '/coverage');
 $cvg->setProcessUncoveredFilesFromWhitelist(FALSE);
