b512b89fe1cc3c614caafc8fcb2936cd9e935cd5
Patrik Votocek <patrik@votocek.cz>
2012-09-01 23:56:26 +0200

    view: no TW Bootstrap forms

--- app/forms/BaseForm.php
+++ app/forms/BaseForm.php
@@ -3,8 +3,7 @@
 namespace NetteAddons;
 
 use Nette\Application\UI\Form,
-	Nette\ComponentModel\IContainer,
-	Nette\Application\UI\Presenter;
+	Nette\ComponentModel\IContainer;
 
 
 /**
@@ -14,12 +13,18 @@ use Nette\Application\UI\Form,
  */
 abstract class BaseForm extends Form
 {
-	/** @var TemplateFactory */
-	private $templateFactory;
-
 	public function __construct(IContainer $parent = NULL, $name = NULL)
 	{
 		parent::__construct($parent, $name);
+
+		$renderer = $this->getRenderer();
+		if ($renderer instanceof \Nette\Forms\Rendering\DefaultFormRenderer) {
+			$renderer->wrappers['form']['container'] = 'div class=form';
+			$renderer->wrappers['controls']['container'] = NULL;
+			$renderer->wrappers['pair']['container'] = 'div class=controls';
+			$renderer->wrappers['control']['container'] = NULL;
+			$renderer->wrappers['label']['container'] = NULL;
+		}
 
 		$this->buildForm();
 	}
@@ -31,43 +36,4 @@ abstract class BaseForm extends Form
 	 * @return void
 	 */
 	protected abstract function buildForm();
-
-
-	public function attached($presenter)
-	{
-		parent::attached($presenter);
-
-		if ($presenter instanceof Presenter) {
-			$this->templateFactory = $presenter->getContext()->templateFactory;
-		}
-	}
-
-
-	public function render()
-	{
-		$args = func_get_args();
-		if (!$args && ($file = $this->getTemplateFile())) {
-			$template = $this->templateFactory->createTemplate($file, $this->getParent());
-			$template->bootstrap = $this->getLayoutTemplateFile();
-			$template->form = $this;
-			$template->render();
-		} else {
-			call_user_func_array('parent::render', $args);
-		}
-	}
-
-
-	protected function getTemplateFile()
-	{
-		$refl = $this->getReflection();
-		$file = dirname($refl->getFileName()) . '/' . lcFirst($refl->getShortName()) . '.latte';
-		return file_exists($file) ? $file : NULL;
-	}
-
-
-	protected function getLayoutTemplateFile()
-	{
-		$baseDir = dirname(dirname($this->getReflection()->getFileName()));
-		return "$baseDir/templates/@form.latte";
-	}
 }
