e4a1c0486da46a0d06a47fdc21a4159830cced15
Patrik Votocek <patrik@votocek.cz>
2012-09-01 17:02:44 +0200

    tests: enable debug mode

--- tests2/boot.php
+++ tests2/boot.php
@@ -7,6 +7,7 @@ use Nette\Config\Configurator;
 use Nette\Loaders\RobotLoader;
 
 $configurator = new Configurator();
+$configurator->setDebugMode();
 $configurator->enableDebugger(__DIR__ . '/temp/log');
 $configurator->setTempDirectory(__DIR__ . '/temp');
 $configurator->createRobotLoader()
