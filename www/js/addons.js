507928d14e131bcfc98992efc53a23257ce66084
Patrik Votocek <patrik@votocek.cz>
2012-09-01 23:55:02 +0200

    view: add addons assets

---
+++ www/js/addons.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('[data-addons-toggle]').each(function(i, el) {
+		$(el).click(function(e) {
+			e.preventDefault();
+
+			$this = $(this);
+			$($this.attr('rel')).slideToggle();
+			hRel = $this.attr('data-addons-toggle');
+			if (hRel) {
+				$(hRel).slideToggle();
+			}
+		});
+	});
+});
