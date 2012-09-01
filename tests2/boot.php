d58028ea2e24e1042d4030645939e375a29f8cdf
Patrik Votocek <patrik@votocek.cz>
2012-09-01 17:01:42 +0200

    tests: add log dir

--- tests2/boot.php
+++ tests2/boot.php
@@ -7,7 +7,7 @@ use Nette\Config\Configurator;
 use Nette\Loaders\RobotLoader;
 
 $configurator = new Configurator();
-$configurator->enableDebugger();
+$configurator->enableDebugger(__DIR__ . '/temp/log');
 $configurator->setTempDirectory(__DIR__ . '/temp');
 $configurator->createRobotLoader()
 	->addDirectory(LIBS_DIR)
