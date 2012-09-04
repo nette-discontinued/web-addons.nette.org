b8b5c1eaceac64f127c90359401ae315128a0575
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-09-04 16:23:34 +0200

    HomepagePresenter: updated to use Inject Autowire

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,23 +2,48 @@
 
 namespace NetteAddons;
 
+use NetteAddons\Model\Addons;
+use NetteAddons\Model\Reinstall;
+
+
+
 class HomepagePresenter extends BasePresenter
 {
+	/** @var Addons */
+	private $addons;
+
+	/** @var Reinstall */
+	private $reinstaller;
+
+
+
+	public function injectAddons(Addons $addons)
+	{
+		$this->addons = $addons;
+	}
+
+
+
+	public function injectReinstaller(Reinstall $reinstaller)
+	{
+		$this->reinstaller = $reinstaller;
+	}
+
+
 
 	public function renderDefault()
 	{
-		$addons = $this->context->addons;
-
-		$this->template->updatedAddons = $addons->findAll()->order('updatedAt DESC')->limit(3);
+		$this->template->updatedAddons = $this->addons->findAll()
+			->order('updatedAt DESC')->limit(3);
 	}
+
 
 
 	public function handleReinstall()
 	{
-		$this->context->reinstall->recreateDatabase();
+		$this->reinstaller->recreateDatabase();
 
 		$this->flashMessage('Fuk yea!');
 		$this->redirect('this');
 	}
-
 }
