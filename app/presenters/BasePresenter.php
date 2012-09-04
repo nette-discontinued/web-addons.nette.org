b06a73e4a0d8b9e8eecec5ae44cd1ef128b597b2
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-09-04 13:12:35 +0200

    BasePresenter: updated to use Inject Autowire

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -2,19 +2,57 @@
 
 namespace NetteAddons;
 
+use NetteAddons\TemplateFactory;
+use NetteAddons\Model;
+use NetteAddons\Model\Authorizator;
+
+
+
 abstract class BasePresenter extends \Nette\Application\UI\Presenter
 {
+	/** @var Authorizator */
+	protected $auth;
+
+	/** @var TemplateFactory */
+	protected $tplFactory;
+
+	/** @var Model\Tags */
+	protected $tags;
+
+
+
+	public function injectAuthorizator(Authorizator $auth)
+	{
+		$this->auth = $auth;
+	}
+
+
+
+	public function injectTemplateFactory(TemplateFactory $factory)
+	{
+		$this->tplFactory = $factory;
+	}
+
+
+
+	public function injectTags(Model\Tags $tags)
+	{
+		$this->tags = $tags;
+	}
+
+
 
 	public function createTemplate($class = NULL)
 	{
-		return $this->context->templateFactory->createTemplate(NULL, $this);
+		return $this->tplFactory->createTemplate(NULL, $this);
 	}
+
+
 
 	protected function beforeRender()
 	{
-		$this->template->categories = $this->context->tags->findMainTags();
-		$this->template->tags = $this->context->tags; // pro praci s tagama
-		$this->template->auth = $this->context->authorizator;
+		$this->template->auth = $this->auth;
+		$this->template->categories = $this->tags->findMainTags();
+		$this->template->tags = $this->tags;
 	}
-
 }
