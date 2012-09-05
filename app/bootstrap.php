0c1027ec612099cbd496276afad894e94ef0b5c1
Patrik Votocek <patrik@votocek.cz>
2012-09-05 23:41:37 +0200

    config: disable force debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -12,7 +12,7 @@ require_once LIBS_DIR . '/autoload.php';
 $configurator = new Configurator;
 
 // Enable Debugger
-$configurator->setDebugMode(TRUE);
+//$configurator->setDebugMode(TRUE);
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 // Enable RobotLoader
