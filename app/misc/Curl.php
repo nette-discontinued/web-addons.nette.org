58aa12c3515fb3b8f277993cee886168c6a3b74c
Michael Moravec <mail@majkl578.cz>
2012-09-29 15:09:22 +0200

    rewritten curl handling - split into Curl and CurlRequest

--- app/misc/Curl.php
+++ app/misc/Curl.php
@@ -3,7 +3,8 @@
 namespace NetteAddons;
 
 /**
- * @author	Patrik Votoček
+ * @author Patrik Votoček
+ * @author Michael Moravec
  */
 class Curl extends \Nette\Object
 {
@@ -17,39 +18,42 @@ class Curl extends \Nette\Object
 	/**
 	 * @param int The maximum number of milliseconds to allow cURL functions to execute.
 	 */
-	public function __construct($timeout = 500)
+	public function __construct($timeout = 0)
 	{
 		if (!extension_loaded('curl')) {
 			throw new \NetteAddons\InvalidStateException('Missing cURL extension');
 		}
 
 		$this->timeout = $timeout;
+
+		if (!defined('CURLOPT_TIMEOUT_MS')) {
+			define('CURLOPT_TIMEOUT_MS', 155);
+		}
 	}
 
 
 
 	/**
-	 * @return resource
+	 * @return CurlRequest
 	 */
-	protected function create()
+	public function create($url = NULL)
 	{
-		if (!defined('CURLOPT_TIMEOUT_MS')) {
-			define('CURLOPT_TIMEOUT_MS', 155);
-		}
+		$request = new CurlRequest($url);
 
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_USERAGENT, self::UA);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_TIMEOUT_MS, $this->timeout);
-		curl_setopt($ch, CURLOPT_FAILONERROR, FALSE);
+		$request->setOption(CURLOPT_USERAGENT, self::UA);
+		$request->setOption(CURLOPT_FOLLOWLOCATION, TRUE);
+		$request->setOption(CURLOPT_RETURNTRANSFER, TRUE);
+		$request->setOption(CURLOPT_FAILONERROR, FALSE);
 
 		if (PHP_OS === 'WINNT') {
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			$request->setOption(CURLOPT_SSL_VERIFYPEER, FALSE);
 		}
 
-		return $ch;
+		if ($this->timeout) {
+			$request->setTimeout($this->timeout);
+		}
+
+		return $request;
 	}
 
 
@@ -59,43 +63,11 @@ class Curl extends \Nette\Object
 	 * @return string
 	 * @throws CurlException if cURL execution fails, see http://curl.haxx.se/libcurl/c/libcurl-errors.html
 	 * @throws HttpException if server returns HTTP code other than 200 OK
+	 *
+	 * @deprecated
 	 */
 	public function get($url)
 	{
-		$ch = $this->create();
-
-		curl_setopt($ch, CURLOPT_URL, (string) $url);
-		curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
-
-		$data = curl_exec($ch);
-		if (($err = curl_errno($ch)) !== CURLE_OK || $data === FALSE) {
-			if ($err !== CURLE_HTTP_NOT_FOUND) { // correct name is CURLE_HTTP_RETURNED_ERROR
-				throw new \NetteAddons\CurlException(curl_error($ch), $err);
-			}
-		}
-
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		if ($httpCode !== 200 || $err === CURLE_HTTP_NOT_FOUND) {
-			throw new \NetteAddons\HttpException("Server returned HTTP code other than 200 OK.", (int) $httpCode);
-		}
-
-		curl_close($ch);
-
-		return $data;
+		return $this->create($url)->execute();
 	}
-}
-
-
-
-/**
- * @link http://curl.haxx.se/libcurl/c/libcurl-errors.html
- */
-class CurlException extends \RuntimeException
-{
-
-}
-
-class HttpException extends \NetteAddons\IOException
-{
-
 }
---
+++ app/misc/CurlRequest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace NetteAddons;
+
+/**
+ * @author Michael Moravec
+ */
+class CurlRequest extends \Nette\FreezableObject
+{
+	/** @var array */
+	private $options = array();
+
+
+
+	public function __construct($url = NULL)
+	{
+		if (!extension_loaded('curl')) {
+			throw new \NetteAddons\InvalidStateException('Missing cURL extension');
+		}
+
+		if ($url !== NULL) {
+			$this->setOption(CURLOPT_URL, (string) $url);
+		}
+	}
+
+
+
+	/**
+	 * @param int $name
+	 * @param mixed $value
+	 * @return CurlRequest
+	 */
+	public function setOption($name, $value)
+	{
+		$this->updating();
+
+		$this->options[$name] = $value;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param int $name
+	 * @return CurlRequest
+	 */
+	public function removeOption($name)
+	{
+		$this->updating();
+
+		unset($this->options[$name]);
+		return $this;
+	}
+
+
+
+	/**
+	 * @return string
+	 * @throws CurlException if cURL execution fails, see http://curl.haxx.se/libcurl/c/libcurl-errors.html
+	 * @throws HttpException if server returns HTTP code other than 200 OK
+	 */
+	public function execute()
+	{
+		$this->freeze();
+
+		$ch = curl_init();
+
+		curl_setopt_array($ch, $this->options);
+
+		$data = curl_exec($ch);
+
+
+		if (($err = curl_errno($ch)) !== CURLE_OK || $data === FALSE) {
+			if ($err !== CURLE_HTTP_NOT_FOUND) {// correct name is CURLE_HTTP_RETURNED_ERROR
+				throw new \NetteAddons\CurlException(curl_error($ch), $err);
+			}
+		}
+
+		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if ($httpCode !== 200 || $err === CURLE_HTTP_NOT_FOUND) {
+			throw new \NetteAddons\HttpException("Server returned HTTP code other than 200 OK.", (int) $httpCode);
+		}
+
+		curl_close($ch);
+
+		return $data;
+	}
+
+
+
+	/******* shortcuts ********/
+
+
+
+	/**
+	 * @param int $timeout timeout in milliseconds
+	 * @return CurlRequest
+	 */
+	public function setTimeout($timeout)
+	{
+		$this->setOption(CURLOPT_TIMEOUT_MS, $timeout);
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string $method valid HTTP 1.0 method
+	 * @return CurlRequest
+	 */
+	public function setMethod($method)
+	{
+		$this->removeOption(CURLOPT_HTTPGET);
+		$this->setOption(CURLOPT_CUSTOMREQUEST, $method);
+
+		return $this;
+	}
+}
+
+
+
+/**
+ * @link http://curl.haxx.se/libcurl/c/libcurl-errors.html
+ */
+class CurlException extends \RuntimeException
+{
+
+}
+
+class HttpException extends \NetteAddons\IOException
+{
+
+}
