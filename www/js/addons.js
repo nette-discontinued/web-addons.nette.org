8c16d29ebec6c7ac428b25b22a65a865d06d197c
Patrik Votocek <patrik@votocek.cz>
2012-09-29 17:05:18 +0200

    update css and js

--- www/js/addons.js
+++ www/js/addons.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-	$('[data-addons-toggle]').each(function(i, el) {
-		$(el).click(function(e) {
-			e.preventDefault();
+require(['jquery'], ['netteForms'], function($) {
+	$(document).ready(function() {
+		$('[data-addons-toggle]').each(function(i, el) {
+			$(el).click(function(e) {
+				e.preventDefault();
 
-			$this = $(this);
-			$($this.attr('rel')).slideToggle();
-			hRel = $this.attr('data-addons-toggle');
-			if (hRel) {
-				$(hRel).slideToggle();
-			}
+				$this = $(this);
+				$($this.attr('rel')).slideToggle();
+				hRel = $this.attr('data-addons-toggle');
+				if (hRel) {
+					$(hRel).slideToggle();
+				}
+			});
 		});
 	});
 });
