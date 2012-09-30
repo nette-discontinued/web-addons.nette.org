d2a63e0dd17661c0fb324cce8b4f2afd2a423439
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-09-30 09:48:14 +0200

    addons.js: added auto anchor generation from nette.org

--- www/js/addons.js
+++ www/js/addons.js
@@ -15,5 +15,9 @@ $(document).ready(function() {
 
 	$.nette.init();
 
+	$("#content h2[id|=toc], #content h3[id|=toc]").each(function () {
+		$(this).append($('<a class="anchor">#</a>').attr("href", "#" + $(this).attr("id")));
+	});
+
 	$(".chzn-select").chosen();
 });
