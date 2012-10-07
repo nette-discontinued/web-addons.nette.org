63c7385d9ea0c37923fbc621720a6fc84c921975
Patrik Votocek <patrik@votocek.cz>
2012-10-07 07:31:10 +0200

    add FormControl

---
+++ app/forms/FormControl.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace NetteAddons;
+
+use Nette;
+use Nette\Forms\Form;
+
+
+/**
+ * @author Patrik Votoček
+ */
+abstract class FormControl extends Nette\Application\UI\Control
+{
+
+	/** @var array */
+	public $onSuccess;
+
+
+	/**
+	 * @param \Nette\Forms\Form
+	 */
+	protected function doOnSuccess(Form $form)
+	{
+		if ($form->valid) {
+			callback($this, 'onSuccess')->invokeArgs(func_get_args());
+		}
+	}
+
+}
