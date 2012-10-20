e87af1d862924d57bbe92867f5c35e0d22d2c87b
Patrik Votocek <patrik@votocek.cz>
2012-10-20 17:00:44 +0200

    update error presenter

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -2,18 +2,14 @@
 
 namespace NetteAddons;
 
-use Nette\Diagnostics\Debugger,
-	Nette\Application as NA;
+use Nette\Diagnostics\Debugger;
 
 
 
 /**
  * Error presenter.
- *
- * @author     John Doe
- * @package    MyApplication
  */
-class ErrorPresenter extends BasePresenter
+class ErrorPresenter extends \Nette\Application\UI\Presenter
 {
 
 	/**
@@ -26,8 +22,11 @@ class ErrorPresenter extends BasePresenter
 			$this->payload->error = TRUE;
 			$this->terminate();
 
-		} elseif ($exception instanceof NA\BadRequestException) {
+		} elseif ($exception instanceof \Nette\Application\BadRequestException) {
 			$code = $exception->getCode();
+			if ($code != 500) {
+				$this->template->registerHelperLoader($this->getContext()->getByType('NetteAddons\HelperLoader'));
+			}
 			// load template 403.latte or 404.latte or ... 4xx.latte
 			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
 			// log to access.log
