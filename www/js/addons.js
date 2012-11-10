7b57d3bbc9d65d97ad4e8aaa6c43d3d60819f444
Patrik Votocek <patrik@votocek.cz>
2012-11-10 18:22:07 +0100

    Hopepage: search

--- www/js/addons.js
+++ www/js/addons.js
@@ -39,6 +39,57 @@ $(document).ready(function() {
 
 	$(".chzn-select").chosen();
 
+
+	if ($("input.addons-search").length > 0) {
+		var addons = [];
+
+		var $table = $('<table class="table-full"></table>');
+		var $result = $('<div class="addons-search-result"></div>');
+		$result.append($('<h2>Results</h2>')).append($table).hide();
+		var $list = $('.addons-categorized-list');
+		$list.after($result);
+
+		$("tr.addon").each(function(i, el) {
+			if (typeof addons[el.id] == "undefined") {
+				var $el = $(el);
+				var id = $el.attr('data-addon-id')
+				addons['addon-'+id] = $el.attr('data-addon-name');
+				$table.append($el.clone().attr('id', 'addon-'+id));
+			}
+		});
+
+		$("input.addons-search").keyup(function(e) {
+			$el = $(e.target);
+			queryRegex = '';
+			queryString = $el.val();
+			if (queryString.length < 1) {
+				$list.show();
+				$result.hide();
+				return;
+			} else {
+				$list.hide();
+				$result.show();
+			}
+			parts = queryString.split(' ');
+			for (var i=0;i<parts.length;i++) {
+				if (queryRegex.length > 0) {
+					queryRegex += '(.*)';
+				}
+				queryRegex += parts[i].replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+			}
+			regexp = new RegExp(queryRegex, 'i');
+			for (var id in addons) {
+				if (regexp.test(addons[id])) {
+					$('#'+id).show();
+				} else {
+					$('#'+id).hide();
+				}
+			};
+		});
+	}
+
+
+
 	var $downloadsGraph = $('#downloads-graph');
 	if ($downloadsGraph.length) {
 		google.load('visualization', '1.0', { 'packages' : ['corechart'], 'callback' : function () {
