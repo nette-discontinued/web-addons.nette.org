090c7bb8b485a8f301cebdecd445dc4d95a2dce1
Patrik Votocek <patrik@votocek.cz>
2012-11-12 04:55:07 +0100

    update maintenance

--- www/index.php
+++ www/index.php
@@ -9,8 +9,8 @@ define('APP_DIR', WWW_DIR . '/../app');
 // absolute filesystem path to the libraries
 define('LIBS_DIR', WWW_DIR . '/../libs');
 
-// uncomment this line if you must temporarily take down your site for maintenance
-// require APP_DIR . '/templates/maintenance.phtml';
+// Uncomment this line if you must temporarily take down your site for maintenance.
+// require __DIR__ . '/.maintenance.php';
 
 // load bootstrap file
 require APP_DIR . '/bootstrap.php';
