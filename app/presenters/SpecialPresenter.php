e8c6040992e4fd4d46162708ffbb784b31b8d13e
Patrik Votocek <patrik@votocek.cz>
2014-03-13 21:47:58 +0100

    Special presenter now use texy files as pages data source

--- app/presenters/SpecialPresenter.php
+++ app/presenters/SpecialPresenter.php
@@ -2,6 +2,8 @@
 
 namespace NetteAddons;
 
+use Nette\Utils\Finder;
+use Nette\Utils\Strings;
 
 final class SpecialPresenter extends BasePresenter
 {
@@ -10,6 +12,26 @@ final class SpecialPresenter extends BasePresenter
 	 * @var \NetteAddons\Model\Addons
 	 */
 	public $addons;
+
+	/**
+	 * @inject
+	 * @var \NetteAddons\TextProcessors\PageProcessor
+	 */
+	public $pageProcessor;
+
+	/** @var string */
+	private $pagesDataPath;
+
+
+	/**
+	 * @param string
+	 */
+	public function __construct($pagesDataPath)
+	{
+		parent::__construct();
+
+		$this->pagesDataPath = realpath($pagesDataPath . '/page');
+	}
 
 
 	/**
@@ -20,6 +42,23 @@ final class SpecialPresenter extends BasePresenter
 		$this->template->addons = $this->addons->findAll();
 		$this->template->vendors = $this->addons->findVendors();
 		$this->template->categories = $this->tags->findMainTags();
+
+		$pages = array();
+		foreach (Finder::findFiles('*.texy')->from($this->pagesDataPath) as $file) {
+			/** @var \SplFileInfo $file */
+			$slug = Strings::substring($file->getRealPath(), Strings::length($this->pagesDataPath) + 1, -5);
+			$data = $this->pageProcessor->process(file_get_contents($file->getRealPath()));
+			$createdAt = new \DateTime;
+			$createdAt->setTimestamp($file->getMTime());
+			$pages[] = (object) array(
+				'slug' => $slug,
+				'name' => $data['title'],
+				'createdAt' => $createdAt,
+			);
+		}
+
+		$this->template->pages = $pages;
+
 
 		if ($type == 'xml') {
 			$this->setView('sitemap.xml');
