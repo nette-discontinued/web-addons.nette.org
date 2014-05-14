3bdaa331b9a495351b71c3ea7c579032e5adc7d5
Patrik Votocek <patrik@votocek.cz>
2014-05-14 18:41:47 +0200

    Better maintenance switch

--- www/index.php
+++ www/index.php
@@ -1,7 +1,9 @@
 <?php
 
 // Uncomment this line if you must temporarily take down your site for maintenance.
-// require '.maintenance.php';
+if (PHP_SAPI !== 'cli' && file_exists(__DIR__ . '/../maintenance')) {
+	require __DIR__ . '/.maintenance.php';
+}
 
 // Let bootstrap create Dependency Injection container.
 $container = require __DIR__ . '/../app/bootstrap.php';
